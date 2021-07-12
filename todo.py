3e298f4d539f535a17a18df456f5af6c0298f2e1
vniversvs <vniversvs@gmail.com>
2021-07-12 12:20:16 -0300

    first commit

---
+++ todo.py
@@ -0,0 +1,46 @@
+############## RESEARCH
+#      ( )  Gather metadata about mouse tracking
+##     ( )  Usual data sites
+##     ( )  Scientific
+##     ( )  Communities
+##     ( )  
+##     ( )
+##     ( )
+###    ( )
+#      ( )  Theoretical discussion of the problem
+##     ( )  categorization
+###    ( )
+#      ( )
+##     ( )
+###    ( )
+############## DATA PHASE
+#      ( )  Gather Data
+##     ( )
+###    ( )
+#      ( )  Organize Data
+##     ( )
+###    ( )
+############## DEVELOPMENT
+#      ( )
+##     ( )
+###    ( )
+#      ( )
+##     ( )
+###    ( )
+############## DEPLOYMENT
+#      ( )
+##     ( )
+###    ( )
+#      ( )
+##     ( )
+###    ( )
+
+
+
+
+
+
+
+#      ( )
+##     ( )
+###    ( )
